081fc50cd65eadb72cc278b3515bb13ab46dd31c
ka-te-vey <oscar19092019@gmail.com>
2026-07-20 11:49:09 +0700

    fixed controller and validate

--- Server/middleware/validation.js
+++ Server/middleware/validation.js
@@ -1,12 +1,6 @@
 const Joi = require('joi');
 
 exports.signinSchema = Joi.object({
-    name: Joi.string()
-    .required()
-    .min(5)
-    .max(40),
-
-
     email: Joi.string()
     .required()
     .email({ tlds: { allow: ['com'] } }),
@@ -14,9 +8,24 @@ exports.signinSchema = Joi.object({
 
     password: Joi.string()
     .required()
+});
+
+
+exports.signupSchema = Joi.object({
+    name: Joi.string()
+    .required()
+    .min(5)
+    .max(30),
+
+    email: Joi.string()
+    .required()
+    .email({ tlds: { allow: ['com'] } }),
+
+    password: Joi.string()
+    .required()
     .min(8)
-    .max(25)
-    .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'))
+    .max(20)
+    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,25}$'))
     .message({
         'string.pattern.base':
         'Password must be at least 8 characters and contain one Capital case, Lower case and numbers'
@@ -24,18 +33,9 @@ exports.signinSchema = Joi.object({
 });
 
 
-exports.signupSchema = Joi.object({
+exports.acceptCodeSchema = Joi.object({
     email: Joi.string()
-    .required()
-    .email({ tlds: { allow: ['com'] } }),
-
-    password: Joi.string()
-    .required()
-});
-
-
-exports.acceptCodeSchma = Joi.object({
-    email: Joi.string(),
+    .required(),
 
     provideCode: Joi.number()
     .required()
@@ -45,7 +45,7 @@ exports.acceptCodeSchma = Joi.object({
 exports.changePassword = Joi.object({
     newPassword: Joi.string()
     .required()
-    .pattern(new RegExp('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$'))
+    .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,25}$'))
     .message({
         'string.pattern.base':
         'Password must be at least 8 characters and contain one Capital case, Lower case and numbers'
--- Server/route/authRoute.js
+++ Server/route/authRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require('../controller/authController');
+const { identifier } = require('../middleware/identification');
 const Router = express.Router();
 
 
@@ -7,13 +8,13 @@ Router.post('/Signin', authController.signin);
 Router.post('/Signup', authController.signup);
 Router.post('/Signout', authController.signout);
 
-Router.patch('/send-verification-code', authController.sendVerificationCode);
-Router.patch('/verify-verification-code', authController.verifyVerificationCode);
+Router.patch('/Send-verification-code', authController.sendVerificationCode);
+Router.patch('/Verify-verification-code', authController.verifyVerificationCode);
 
-Router.patch('/change-password', authController.changePassword);
+Router.patch('/Change-password', identifier,authController.changePassword);
 
-Router.patch('/forgot-password', authController.sendForgotPasswordCode);
-Router.patch('/new-password', authController.verifyForgotPasswordCode);
+Router.patch('/Forgot-password', authController.sendForgotPasswordCode);
+Router.patch('/New-password', authController.verifyForgotPasswordCode);
 
 
 module.exports = Router;
